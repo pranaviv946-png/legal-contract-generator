cc4b4466515f8cba0aceddccc24c567c72d7855d
shambhavi05-yadav <saishambhavi05@gmail.com>
2026-03-21 11:49:42 +0530

    Complete frontend with backend integration and translation fix

---
+++ server.py
@@ -0,0 +1,7 @@
+from flask import Flask, send_file
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return send_file('index (2).html')
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
